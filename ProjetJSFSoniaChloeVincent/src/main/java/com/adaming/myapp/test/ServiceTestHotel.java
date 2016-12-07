a0b1de2172c0a9a55dce2eb0fd0f5675d9425c77
inti0408 <inti0408@DESKTOP-HSTCJB6>
2016-12-07 11:49:47 +0100

    commit package test

--- ProjetJSFSoniaChloeVincent/src/main/java/com/adaming/myapp/test/ServiceTestHotel.java
+++ ProjetJSFSoniaChloeVincent/src/main/java/com/adaming/myapp/test/ServiceTestHotel.java
@@ -1,5 +1,69 @@
 package com.adaming.myapp.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class ServiceTestHotel {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testAddHotel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHotel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEmployesByHotel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClientsByHotel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProduitsByHotel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProduitsDisposByHotel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBeneficeAnnuel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChambreByHotel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChambreDisposByHotel() {
+		fail("Not yet implemented");
+	}
 
 }
--- ProjetJSFSoniaChloeVincent/src/main/java/com/adaming/myapp/test/ServiceTestProduit.java
+++
@@ -1,5 +0,0 @@
-package com.adaming.myapp.test;
-
-public class ServiceTestProduit {
-
-}
