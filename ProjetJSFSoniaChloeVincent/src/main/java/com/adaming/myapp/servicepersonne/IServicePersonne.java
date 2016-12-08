12c2dba368c2e6087a24c8afcfd7174e262d2b6b
vincentmouly <vincentmouly@gmail.com>
2016-12-08 16:27:32 +0100

    Commit Vincent liste conso/reserv non facturees 16h27

--- ProjetJSFSoniaChloeVincent/src/main/java/com/adaming/myapp/servicepersonne/IServicePersonne.java
+++ ProjetJSFSoniaChloeVincent/src/main/java/com/adaming/myapp/servicepersonne/IServicePersonne.java
@@ -3,14 +3,31 @@ package com.adaming.myapp.servicepersonne;
 import java.util.List;
 
 
+import java.util.Set;
+
+import com.adaming.myapp.entities.Consomation;
 import com.adaming.myapp.entities.Personne;
+import com.adaming.myapp.entities.Reservation;
 
 public interface IServicePersonne {
 
 	
 
 	Personne addPersonne(Personne P, Long idHotel);
+	
 	Personne getPersonne(Long idPersonne);
+	
 	List<Personne> getAll();
+	
 	Personne updatePersonne(Personne P);
+	
+	Set<Reservation> reservations(Long idPersonne);
+	
+	Set<Reservation> reservationsNonFacturees(Long idPersonne);
+	
+	Set<Consomation> consomations(Long idPersonne);
+	
+	Set<Consomation> consomationsNonFacturees(Long idPersonne);
+	
+	
 }
--- ProjetJSFSoniaChloeVincent/src/main/java/com/adaming/myapp/servicepersonne/ServicePersonneImpl.java
+++ ProjetJSFSoniaChloeVincent/src/main/java/com/adaming/myapp/servicepersonne/ServicePersonneImpl.java
@@ -1,14 +1,22 @@
 package com.adaming.myapp.servicepersonne;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 
 
 
+import java.util.Set;
+
 import org.springframework.transaction.annotation.Transactional;
 
 import com.adaming.myapp.daopersonne.IDaoPersonne;
+import com.adaming.myapp.entities.Consomation;
+import com.adaming.myapp.entities.Facture;
+import com.adaming.myapp.entities.Hotel;
 import com.adaming.myapp.entities.Personne;
+import com.adaming.myapp.entities.Reservation;
 @Transactional
 public class ServicePersonneImpl implements IServicePersonne {
 	
@@ -44,6 +52,78 @@ public class ServicePersonneImpl implements IServicePersonne {
 	public Personne updatePersonne(Personne P) {
 		// TODO Auto-generated method stub
 		return dao.updatePersonne(P);
+	}
+
+
+
+
+	@Override
+	public Set<Reservation> reservations(Long idPersonne) {
+		Personne p = dao.getPersonne(idPersonne);
+		
+		return p.getReservations();
+	}
+
+
+
+
+	@Override
+	public Set<Reservation> reservationsNonFacturees(Long idPersonne) {
+		Personne p = dao.getPersonne(idPersonne);
+		
+		Set<Reservation> reservations = reservations(idPersonne);
+		Set<Reservation> reservationsNonFacturees = new HashSet<Reservation>();
+		
+		for (Reservation r : reservations) {
+			if (r.getFacture() == null) {
+				reservationsNonFacturees.add(r);
+			}
+		}
+		
+		return reservationsNonFacturees;
+	}
+
+
+
+
+	@Override
+	public Set<Consomation> consomations(Long idPersonne) {
+		Personne p = dao.getPersonne(idPersonne);
+		
+		return p.getConsomations();
+	}
+
+
+
+
+	@Override
+	public Set<Consomation> consomationsNonFacturees(Long idPersonne) {
+		
+		Personne p = dao.getPersonne(idPersonne);
+		
+		Set<Reservation> reservations = p.getReservations();
+		Iterator<Reservation> iter = reservations.iterator();
+		Reservation reserv = iter.next();
+		
+		Hotel h = reserv.getHotel();
+		
+		Set<Consomation> consomations = consomations(idPersonne);
+		Set<Consomation> consomationsNonFacturees = new HashSet<Consomation>();
+		
+		
+		
+		for (Consomation c : consomations) {
+			for (Facture f : h.getFactures()) {
+				for (Consomation conso : f.getConsomation()) {
+					if (conso.getIdConsommation() == c.getIdConsommation()) {
+						consomationsNonFacturees.add(c);
+					}
+				}
+			}
+		}
+		
+		return consomationsNonFacturees;
+		
 	}
 
 
