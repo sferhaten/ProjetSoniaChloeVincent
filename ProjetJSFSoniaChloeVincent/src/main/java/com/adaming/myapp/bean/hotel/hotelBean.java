7d6913e01a3d4e34cbe6e4a5e5340a56d06d17ec
inti0408 <inti0408@DESKTOP-HSTCJB6>
2016-12-09 14:43:14 +0100

    commit avec hotelBean

--- ProjetJSFSoniaChloeVincent/src/main/java/com/adaming/myapp/bean/hotel/hotelBean.java
+++ ProjetJSFSoniaChloeVincent/src/main/java/com/adaming/myapp/bean/hotel/hotelBean.java
@@ -1,15 +1,26 @@
 package com.adaming.myapp.bean.hotel;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
 import org.jboss.logging.Logger;
+import org.springframework.stereotype.Component;
 
+import com.adaming.myapp.entities.Chambre;
 import com.adaming.myapp.entities.Hotel;
 import com.adaming.myapp.servicehotel.IServiceHotel;
 
+
+
+@Component("hotelBean")
+@ViewScoped
 public class hotelBean {
 
 	private Logger LOGGER = Logger.getLogger(hotelBean.class);
@@ -18,8 +29,19 @@ public class hotelBean {
 	private IServiceHotel serviceHotel;
 	
 	private List<Hotel> hotels = new ArrayList<Hotel>();
+	private Set<Chambre> chambres = new HashSet<Chambre>();
 	
 	
+	public Set<Chambre> getChambres() {
+		return chambres;
+	}
+
+
+	public void setChambres(Set<Chambre> chambres) {
+		this.chambres = chambres;
+	}
+
+
 	public List<Hotel> getHotels() {
 		return hotels;
 	}
@@ -29,10 +51,28 @@ public class hotelBean {
 		this.hotels = hotels;
 	}
 
-
+@PostConstruct
 	public List<Hotel> getAll(){
 		 hotels = serviceHotel.getAll();
 		 return hotels;
 	}
+
+public Set<Chambre> getChambreSimpleDispo(Long idHotel, Date dateDemandee1, Date dateDemandee2){
+	chambres = serviceHotel.chambreSimpleDisposByHotel(idHotel, dateDemandee1, dateDemandee2);
+	
+	return chambres;
+}
+
+public Set<Chambre> getChambreDoubleDispo(Long idHotel, Date dateDemandee1, Date dateDemandee2){
+	chambres = serviceHotel.chambreDoubleDisposByHotel(idHotel, dateDemandee1, dateDemandee2);
+	
+	return chambres;
+}
+public Set<Chambre> getChambreSuiteDispo(Long idHotel, Date dateDemandee1, Date dateDemandee2){
+	chambres = serviceHotel.chambreSuiteDisposByHotel(idHotel, dateDemandee1, dateDemandee2);
+	
+	return chambres;
+}
+
 	
 }
